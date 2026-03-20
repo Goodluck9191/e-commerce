9884e472df76c75a3a8a7e7c7bc23c8cfd813fc3
Goodluck <goodluckprosper76@gmail.com>
2026-03-20 11:45:03 +0300

    Add the navbar, hero section and Products

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
+import { Link, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
-import { Link, Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import About from './pages/About'
+import ProfilePage from "./pages/ProfilePage";
+import LoginPage from "./pages/LoginPage";
+import Navbar from "./components/Navbar";
+import { UserProvider } from "./context/UserContextProvider";
 
 export default function App() {
-  return (
-    <div>
-      <nav>
-        <Link to={'/'}>Home</Link>
-        <Link to={'/about'}>About</Link>
-      </nav>
-      <Routes>
-      <Route path='/' element={< HomePage/>} />
-      <Route path='/about' element={<About />} />
-      <Route path='*' element={<h1>404 not Found</h1>}/>
-      </Routes>
+	return (
+		<UserProvider>
+			<div className="home">
+				<Navbar />
 
-      <div>
-        Footer
-      </div>
-      
-    </div>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/profile" element={<ProfilePage />} />
+					<Route path="login" element={<LoginPage />} />
+					<Route path="*" element={<h1>404 not Found</h1>} />
+				</Routes>
 
-  )
+				<div>Footer</div>
+			</div>
+		</UserProvider>
+        
+	);
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import Products from "../components/Products";
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+	return (
+		<>
+			<section className="hero">
+				<div className={"announce"}>
+					<div className={"dot"}>
+						<div className={"pulse"}></div>
+					</div>
 
-export default HomePage
+					<p>Introducing GoodyShop 2.0</p>
+				</div>
+
+				<h1>Welcome to GoodyShop</h1>
+				<p className={"subtitle"}>
+					Discover the amazing Products with great Prices
+				</p>
+			</section>
+
+			<Products />
+		</>
+	);
+};
+
+export default HomePage;
