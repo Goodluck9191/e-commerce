05887516aab3425438516fdcf1a47dbcb6490f40
Goodluck Prosper <goodluckprosper76@gmail.com>
2026-03-22 16:13:20 +0300

    Revert "E commerce"

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 
-import ProfilePage from "./pages/ProfilePage";
-import AuthPage from "./pages/AuthPage";
-import AuthProvider from "./context/AuthProvider";
+import { Link, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import About from './pages/About'
 
 export default function App() {
-	return (
-		<div className="hero">
-			<AuthProvider>
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/profile" element={<ProfilePage />} />
-					<Route path="/auth" element={<AuthPage />} />
-					<Route path="*" element={<h1>404 not Found</h1>} />
-				</Routes>
-			</AuthProvider>
-		</div>
-	);
+  return (
+    <div>
+      <nav>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/about'}>About</Link>
+      </nav>
+      <Routes>
+      <Route path='/' element={< HomePage/>} />
+      <Route path='/about' element={<About />} />
+      <Route path='*' element={<h1>404 not Found</h1>}/>
+      </Routes>
+
+      <div>
+        Footer
+      </div>
+      
+    </div>
+
+  )
 }
